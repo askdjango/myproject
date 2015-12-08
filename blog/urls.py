73d455495a90fa8babdb8a5b7a4f29c6191328ac
Chinseok Lee <allieuslee@gmail.com>
2015-12-08 09:59:25 +0900

    라이브 코딩쇼 #04 내역

--- blog/urls.py
+++ blog/urls.py
@@ -1,17 +1,19 @@
 from django.conf.urls import url, patterns
+from . import views
 
-urlpatterns = patterns('blog.views',
-    url(r'^$', 'index', name='index'),  # 글 목록
-    url(r'^(?P<pk>\d+)/$', 'post_detail', name='post_detail'),  # 글 내용
+
+urlpatterns = [
+    url(r'^$', views.index, name='index'),  # 글 목록
+    url(r'^(?P<pk>\d+)/$', views.post_detail, name='post_detail'),  # 글 내용
 
     # post create
-    url(r'^new/$', 'post_create', name='post_create'),
+    url(r'^new/$', views.post_create, name='post_create'),
     # post update
-    url(r'^(?P<pk>\d+)/edit/$', 'post_update', name='post_update'),
+    url(r'^(?P<pk>\d+)/edit/$', views.post_update, name='post_update'),
     # post delete
-    url(r'^(?P<pk>\d+)/delete/$', 'post_delete', name='post_delete'),
+    url(r'^(?P<pk>\d+)/delete/$', views.post_delete, name='post_delete'),
 
-    url(r'^(?P<pk>\d+)/comments/new/$', 'comment_new', name='comment_new'),  # 새 댓글 생성
-    url(r'^(?P<post_pk>\d+)/comments/(?P<pk>\d+)/edit/$', 'comment_edit', name='comment_edit'),  # 새 댓글 수정
-    url(r'^(?P<post_pk>\d+)/comments/(?P<pk>\d+)/delete/$', 'comment_delete', name='comment_delete'),  # 새 댓글 삭제
-)
+    url(r'^(?P<pk>\d+)/comments/new/$', views.comment_new, name='comment_new'),  # 새 댓글 생성
+    url(r'^(?P<post_pk>\d+)/comments/(?P<pk>\d+)/edit/$', views.comment_edit, name='comment_edit'),  # 새 댓글 수정
+    url(r'^(?P<post_pk>\d+)/comments/(?P<pk>\d+)/delete/$', views.comment_delete, name='comment_delete'),  # 새 댓글 삭제
+]
--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,44 @@
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.urlresolvers import reverse, reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+
 from django.shortcuts import render, redirect
 from .models import Post, Comment
 from .forms import PostForm, CommentForm
 
 
+'''
 def index(request):
     post_list = Post.objects.all()
     return render(request, 'blog/index.html', {
         'post_list': post_list,
     })
+'''
 
 
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/index.html'
+
+# index = ListView.as_view(model=Post, template_name='blog/index.html')
+index = PostListView.as_view()
+
+
+
+'''
 def post_detail(request, pk):
     post = Post.objects.get(pk=pk)  # Post.DoesNotExist
     return render(request, 'blog/post_detail.html', {
         'post': post,
     })
+'''
+
+post_detail = DetailView.as_view(model=Post)  #, pk_url_kwarg='id', template_name='blog/other_detail.html')
 
 
+
+'''
 @login_required
 def post_create(request):
     # request.GET
@@ -40,8 +61,29 @@ def post_create(request):
     return render(request, 'blog/post_form.html', {
         'form': form,
     })
+'''
 
 
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    form_class = PostForm
+    # success_url = reverse_lazy('blog:index')
+
+    def form_valid(self, form):
+        post = form.save(commit=False)
+        post.author = self.request.user
+        post.save()
+        return super(PostCreateView, self).form_valid(form)
+
+    def get_success_url(self):
+        return reverse('blog:post_detail', args=[self.object.pk])
+
+# post_create = login_required(PostCreateView.as_view())
+post_create = PostCreateView.as_view()
+
+
+
+'''
 @login_required
 def post_update(request, pk):
     post = Post.objects.get(pk=pk)
@@ -59,8 +101,20 @@ def post_update(request, pk):
     return render(request, 'blog/post_form.html', {
         'form': form,
     })
+'''
+
+class PostUpdateView(LoginRequiredMixin, UpdateView):
+    model = Post
+    form_class = PostForm
+
+    def get_success_url(self):
+        return reverse('blog:post_detail', args=[self.object.pk])
+
+# post_update = login_required(PostUpdateView.as_view())
+post_update = PostUpdateView.as_view()
 
 
+'''
 @login_required
 def post_delete(request, pk):
     post = Post.objects.get(pk=pk)
@@ -74,6 +128,19 @@ def post_delete(request, pk):
     return render(request, 'blog/post_delete_confirm.html', {
         'post': post,
     })
+'''
+
+class PostDeleteView(LoginRequiredMixin, DeleteView):
+    model = Post
+    success_url = reverse_lazy('blog:index')
+
+    def dispatch(self, request, *args, **kwargs):
+        if self.get_object().author != request.user:
+            return redirect('blog:post_detail', self.kwargs['pk'])
+        return super(PostDeleteView, self).dispatch(request, *args, **kwargs)
+
+# post_delete = login_required(PostDeleteView.as_view())
+post_delete = PostDeleteView.as_view()
 
 
 @login_required
